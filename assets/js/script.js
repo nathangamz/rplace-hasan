1f34c14902306eaf58b7bd6f27e1905900ef9aac
Nathaniel <nathangamz0@gmail.com>
2022-04-03 04:57:07 -0400

    More of the same :(

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,7 +53,8 @@ function place_square() {
             pos.style.left = x * 8 + 16 + "px";
             pos.style.top = y * 8 - 6 + "px";
           }
-        square.addEventListener("mouseover", onTileHovered(cx, cy), false);
+        square.addEventListener("mouseover", onTileHovered(row_number, col_number), false);
+
 
         canvas_row.append(square);
     }
