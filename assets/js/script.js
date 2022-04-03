9b8dcdd0a8bfa2b66a1f68e1043040e13d4d6b49
Nathaniel <nathangamz0@gmail.com>
2022-04-03 18:48:55 -0400

    added both hasbulla and cussy as maps

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,28 +9,38 @@ var rplace = {
     initial_y: 1345,
 }
 
-var canvas = {
+var canvas_cussy = {
     width: 128,
     height: 64
 }
 
-var flag_pixelart = null;
-flag_pixelart_json = $.getJSON('assets/json/hasbulla.json', function (data) {
-    flag_pixelart = data;
-    place_square();
+var canvas_hasbulla = {
+    width: 250,
+    height: 250
+}
+
+var cussy_pixelart = null;
+cussy_pixelart_json = $.getJSON('assets/json/cussy.json', function (data) {
+    cussy_pixelart = data;
+    place_square(canvas_cussy.width, cussy_pixelart, "place_canvas_cussy");
+});
+var hasbulla_pixelart = null;
+hasbulla_pixelart_json = $.getJSON('assets/json/hasbulla.json', function (data) {
+    hasbulla_pixelart = data;
+    place_square(canvas_hasbulla.width, hasbulla_pixelart, "place_canvas_hasbulla");
 });
 
 
-function place_square() {
-    var place_canvas = document.getElementById("place_canvas");
+function place_square(width, jsonData, element) {
+    var place_canvas = document.getElementById(element);
 
     var row_number = 0;
     var col_number = 0;
 
-    for (var [onechar, value] of Object.entries(flag_pixelart.map)) {
+    for (var [onechar, value] of Object.entries(jsonData.map)) {
 
         col_number++;
-        if (Number.isInteger(onechar / canvas.width)) {
+        if (Number.isInteger(onechar / width)) {
             col_number = 0;
             row_number++;
             var canvas_row = document.createElement("div");
