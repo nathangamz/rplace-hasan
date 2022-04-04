4e8dea846f53e94f1497b8a83a7279119194a70f
Nathaniel <nathangamz0@gmail.com>
2022-04-03 20:33:05 -0400

    changed coordinates and updated link

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,8 @@ var rplace_hasbulla = {
 }
 
 var rplace_cussy = {
-    initial_x: 748,
-    initial_y: 1177,
+    initial_x: 737,
+    initial_y: 1178,
 }
 
 var canvas_cussy = {
@@ -37,7 +37,7 @@ cussy_pixelart_json = $.getJSON('assets/json/cussy.json', function (data) {
 
 function onTileHovered(x, y) {
     const pos = document.getElementById("pos");
-    pos.innerText = "[" + (x + 737) + ", " + (y + 1177) + "]";
+    pos.innerText = "[" + (x + 737) + ", " + (y + 1178) + "]";
     pos.style.left = x * 8 + 16 + "px";
     pos.style.top = y * 8 - 4 + "px"
   };
@@ -63,7 +63,7 @@ function place_square(width, jsonData, element, rplace) {
         square.style.backgroundColor = "rgb(" + value[0] + "," + value[1] + "," + value[2] + ")"
         var cx = (rplace.initial_x + (col_number + 1));
         var cy = (rplace.initial_y + row_number)
-        var link = `https://www.reddit.com/r/place/?cx=${cx}&cy=${cy}&px=50`
+        var link = `https://www.new.reddit.com/r/place/?cx=${cx}&cy=${cy}&px=50`
         square.setAttribute("onmouseover", "onTileHovered(" + col_number + ", " + row_number + ")");
         square.setAttribute("href", link);
 
